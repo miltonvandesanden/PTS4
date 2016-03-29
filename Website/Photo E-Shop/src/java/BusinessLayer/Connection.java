0c2e3d0b9a0b3023ebfc3da43934a686e1fe6265
Milton van de Sanden <milton.vandesanden@gmail.com>
2016-03-29 12:30:31 +0200

    connection commit
    we commiten de code om te controleren of de connetiefout ook op een
    andere pc plaatsvind

---
+++ Website/Photo E-Shop/src/java/BusinessLayer/Connection.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLayer;
+
+import Package.Database;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Milton
+ */
+public class Connection
+{
+    private Database database;
+    public Connection() throws SQLException
+    {
+        database = new Database();
+    }
+    
+    public boolean CheckLogIn(String username, String password) throws SQLException
+    {
+        boolean success = false;
+        
+        if(username != null && password != null)
+        {
+            //String LogInQuery = "SELECT * FROM Company WHERE username = '" + username + "' AND password = '" + password + "'";
+            String LogInQuery = "SELECT * FROM Company";
+            try
+            {
+                //success = true;
+                if(database.Connect())
+                {
+                    try
+                    {
+                        success = true;
+                        ResultSet resultSet = database.GetQuery("SELECT * FROM Company");
+                        if(resultSet != null)
+                        {
+                            //success = true;
+                            if(resultSet.next())
+                            {
+                                success = true;
+                            }                    
+                        }                        
+                    }
+                    catch(Exception ex)
+                    {
+                        
+                    }
+                    finally
+                    {
+                        //database.Close();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        return success;
+    }
+}
